d10090b19fdba3d7d75fe7812b8f49fb002d02d6
David Marinho <loinedavid34@gmail.com>
2025-04-02 20:38:36 -0300

    add

---
+++ LogicaDeProgramacao/Exercicios/exercicios/for.js
@@ -0,0 +1,37 @@
+/*let numI = 15;
+let numF = 50;
+
+for( let i = numI; i <= numF; i++){
+    if( i % 2 === 1){
+        console.log(`O número ${i} é impar`);
+    } else {
+        console.log(`O número ${i} é par`);
+    }
+}
+    */
+
+
+/*let anoI = 1970
+let anoF = 2025
+
+for(let i = anoI; i <= anoF; i++){
+    if ( i % 4 === 2){
+        console.log(`O ano ${i} foi Copa Do Mundo!`)
+    }
+}
+*/
+
+const prompt = require('prompt-sync') ({sigint: true});
+
+let anoInicial = prompt("Digite um ano de 1930 a 2025: ")
+let anoFinal = 2025;
+
+if (anoInicial < 1930 || anoInicial > 2025){
+    console.log("Digite um ano válido!")
+} else {
+    console.log(`Ano inicial: ${anoInicial}`)
+        if ( i % 4 === 2 && i - 1942 && i - 1946){
+            let ano = i
+            console.log(`${ano} foi um ano de Copa do Mundo!`)
+        }
+    }
